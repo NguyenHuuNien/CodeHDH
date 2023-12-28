8fdac01e349b7d517a58f38f67cb6a1429feb106
Nguyễn Hữu Niên <125124829+NguyenHuuNien@users.noreply.github.com>
2023-12-28 17:40:53 +0700

    chan vai lon

--- thucHanh1.c
+++ thucHanh1.c
@@ -1,19 +1,30 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<unistd.h>
+#include<sys/wait.h>
 int main(){
 	int pid = fork();
+
 	if(pid == -1){
 		fprintf(stderr,"Khong the tao tien trinh!");
 		return 1;
 	}
 	else if(pid > 0){
 		printf("Tien trinh cha (PID = %d)\n",getpid());
+	
 	}
 	else{
 		printf("Tien trinh con (PID = %d)\n",getpid());
+		sleep(2);
+		
 	}
+
 	if(pid>0){
 		printf("Day la tien trinh cha\n");
+		wait(NULL);
+		execlp("ls","-l",(char*)NULL);
+		perror("execl Loi Roi");
+		exit(EXIT_FAILURE);
 	}
 	else{
 		printf("Day la tien trinh con \n");
